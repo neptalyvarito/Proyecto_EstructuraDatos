d8929cd5c0676a49b11106f9f41a93e01f497e39
neptaly <neptalymayodlvega@gmail.com>
2024-11-28 06:42:06 -0500

    listadobledx

---
+++ ConsoleApp1/lista_doble/Nodo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1.Listas
+{
+    public class Nodo<T>
+    {
+        public T Data { get; set; }
+        public Nodo<T> Siguiente { get; set; }
+        public Nodo<T> Anterior { get; set; }
+
+        public Nodo(T data)
+        {
+            Data = data;
+            Siguiente = null;
+            Anterior = null;
+        }
+    }
+}
